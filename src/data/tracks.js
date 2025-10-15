4f9c71178c47275e35a6ac635f765fccb44045a6
clbakerx <clbakerx@gmail.com>
2025-10-15 09:10:26 -0700

    Update tracks.js

--- src/data/tracks.js
+++ src/data/tracks.js
@@ -1,1 +1,7 @@
+// src/data/tracks.js
+export const tracks = [
+  { id: 1, title: "Sample Song", artist: "PulseNexis", src: "/sample.mp3", duration: null },
+  { id: 2, title: "Song Two",    artist: "PulseNexis", src: "/song2.mp3",   duration: null },
+  { id: 3, title: "Song Three",  artist: "PulseNexis", src: "/song3.mp3",   duration: null },
+];
 
